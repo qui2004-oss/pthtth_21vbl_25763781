02cb82eaced5bef37cc751b1850fc24d8ff15747
qui2004-oss <nguyenanhqui202@gmail.com>
2026-09-08 21:10:15 +0700

    test

---
+++ src/Baitap/Bai2.java
@@ -0,0 +1,29 @@
+package Baitap;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Bai2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        InputStream is = System.in;
+        while (true) {
+            try {
+                if (is.available() > 0) {
+                    byte[] buffer = new byte[is.available()];
+                    int bytesRead = is.read(buffer);
+                    if (bytesRead == -1) {
+                        break;
+                    }
+                    String str = new String(buffer, 0, bytesRead);
+                    System.out.print(str);
+                } else {
+                    System.out.print('_');
+                    Thread.sleep(100);
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/Baitap/Bai3.java
@@ -0,0 +1,24 @@
+package Baitap;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        InputStream is = System.in;
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        while (true) {
+            try {
+                String line = br.readLine();
+                if (line == null || line.equals("q")) {
+                    break;
+                }
+                System.out.println(line);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
