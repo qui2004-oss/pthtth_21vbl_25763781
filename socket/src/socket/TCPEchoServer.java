11019f17151f0fcdf98a41666d0b0494090952c0
qui2004-oss <nguyenanhqui202@gmail.com>
2026-09-15 20:24:03 +0700

    Update code TCPEchoServer

---
+++ socket/src/socket/TCPEchoServer.java
@@ -0,0 +1,38 @@
+package socket;
+
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class TCPEchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(6789);
+            System.out.println("SERVER da duoc tao");
+            
+            while (true) {
+                Socket s = ss.accept();
+                
+                InputStream is = s.getInputStream();
+                OutputStream os = s.getOutputStream();
+                
+                int ch = 0;
+                while (true) {
+                    ch = is.read();
+                    if (ch == -1) {
+                        break;
+                    }
+                    System.out.println((char) ch);
+                    os.write(ch);
+                }
+                
+                s.close();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
